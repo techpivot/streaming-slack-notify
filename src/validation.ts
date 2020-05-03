2e85d9335f5cdf2d6a84e02f917c5fd2c6632d85
Mark Johnson <739719+virgofx@users.noreply.github.com>
2020-05-02 22:24:40 -0700

    sadf

--- src/validation.ts
+++ src/validation.ts
@@ -2,7 +2,7 @@ import { NO_GITHUB_TOKEN, NO_SLACK_ACCESS_TOKEN } from './const';
 import { getSlackToken, getGithubToken } from './utils';
 
 export const validateInputs = (): void => {
-  if (getSlackToken().length > 0) {
+  if (getSlackToken() === '') {
     throw new Error(NO_SLACK_ACCESS_TOKEN);
   }
   if (!getGithubToken()) {
