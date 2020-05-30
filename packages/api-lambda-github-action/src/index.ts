746e5b3c537c6f9a29e09fbfa4f147d40d68af6a
Mark Johnson <739719+virgofx@users.noreply.github.com>
2020-05-29 23:00:10 -0700

    cleanup

--- packages/api-lambda-github-action/src/index.ts
+++ packages/api-lambda-github-action/src/index.ts
@@ -1,24 +1,19 @@
 //import { Context, Event, OauthV2AccessResponse } from './types';
+import { APIGatewayProxyResult, Context } from 'aws-lambda';
 import { BaseError } from '../../common/lib/errors';
 
-export const handler = async (event: any, context: any): Promise<any> => {
+
+export const handler = async (event: any, context: Context): Promise<APIGatewayProxyResult> => {
   let statusCode: number = 200;
   let body: object = {};
-  const isBase64Encoded: boolean = false;
-  const cookies: string[] = [];
-  const headers = {
-    Server: 'TechPivot',
-    'Content-Type': 'text/javascript',
-  };
 
   try {
-    statusCode = 200;
     console.log('event', event);
-    console.log('ctx', context);
     //console.log('args', arguments);
     body = {
       test: 'val',
     };
+
   } catch (error) {
     // Log the full error in CloudWatch
     console.error(error);
@@ -29,17 +24,16 @@ export const handler = async (event: any, context: any): Promise<any> => {
     } else {
       statusCode = 500;
     }
-    /*body = parseTemplate('error.html', {
-      errorMessage: error.message || 'Unknown',
-      errorType: error.name || error.code || 'Unknown error type',
-    });*/
+
   } finally {
     return {
       statusCode,
-      isBase64Encoded,
-      cookies,
-      headers,
-      body,
+      isBase64Encoded: false,
+      headers: {
+        Server: 'TechPivot',
+        'Content-Type': 'text/javascript',
+      },
+      body: JSON.stringify(body),
     };
   }
 };
